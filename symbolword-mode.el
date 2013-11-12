1cd454fb7952b07739aa16ba6801b654704c05e8
ncaq <nyrigadake38@gmail.com>
2013-11-13 01:03:42 +0900

    function list

---
+++ symbolword-mode.el
@@ -0,0 +1,11 @@
+;; Key     Word oriented command      Symbolword oriented command
+;; ============================================================
+;; M-f     `forward-word'             `symbolword-forward'
+;; M-b     `backward-word'            `symbolword-backward'
+;; M-@     `mark-word'                `symbolword-mark'
+;; M-d     `kill-word'                `symbolword-kill'
+;; M-DEL   `backward-kill-word'       `symbolword-backward-kill'
+;; M-t     `transpose-words'          `symbolword-transpose'
+;; M-c     `capitalize-word'          `symbolword-capitalize'
+;; M-u     `upcase-word'              `symbolword-upcase'
+;; M-l     `downcase-word'            `symbolword-downcase'
