f6845becfdd4988a3f60744ec505d5b9ea0f86f0
ncaq <ncaq@ncaq.net>
2013-11-13 19:28:13 +0900

    forward,backwardを実装 自分用簡易解説書を書いておくことにする

--- common-function.el
+++ common-function.el
@@ -1,3 +1,11 @@
 (defun equal-type (source syntaxtype)
   (let ((syntax (char-to-string (char-syntax (string-to-char source)))))
     (equal source syntaxtype)))
+
+(defun get-char-from-buffer ()
+  (buffer-substring-no-properties (point) (+ (point) 1)))
+
+(defun get-syntax-from-buffer ()
+  (char-to-string (char-syntax (string-to-char (get-char-from-buffer)))))
+
+(provide 'common-function)
