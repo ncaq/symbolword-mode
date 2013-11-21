515935735b897ad24b47e12e3ce528d9476797cf
ncaq <ncaq@ncaq.net>
2013-11-21 21:32:37 +0900

    全面的に改定した 疲れた…

--- kill-symbolword.el
+++ kill-symbolword.el
@@ -1,27 +1,27 @@
 (require 'common-function)
-
-;; (defun kill-symbolword-sub (str)
-;;   (cond ((equal-syntax-and-unicode str (get-str-from-buffer))
-;; 	 (delete-forward-char 1 t) (kill-symbolword-sub str))))
-
-;; (defun kill-symbolword ()
-;;   (interactive)
-;;   (kill-symbolword-sub (get-str-from-buffer)))
-
-(defun sub-kill-symbolword (startstr)
-  ())
+(require 'cc-cmds)
+(eval-when-compile (require 'cl))
 
 (defun kill-symbolword ()
   (interactive)
-  (sub-kill-symbolword (get-str-from-buffer)))
+  (c-hungry-delete-forward)
+  (kill-forward-chars (kill-symbolword-size (get-str-from-buffer) 1)))
 
-(defun backward-kill-symbolword-sub (str)
-  (cond ((equal-syntax-and-unicode str (get-str-from-buffer-backward))
-	 (delete-char -1 t) (backward-kill-symbolword-sub str))))
+(defun kill-symbolword-size (curr num)
+  (let ((next (get-str-from-buffer-dif num)))
+    (if (not (div-symbolword-forward curr next))
+      (kill-symbolword-size next (incf num))
+    num)))
 
 (defun backward-kill-symbolword ()
   (interactive)
-  (backward-kill-symbolword-sub (get-str-from-buffer-backward)))
+  (c-hungry-delete-backwards)
+  (kill-backward-chars (backward-kill-symbolword-size (get-str-from-buffer-backward) 1)))
 
+(defun backward-kill-symbolword-size (curr num)
+  (let ((next (get-str-from-buffer-dif-backward num)))
+    (if (not (div-symbolword-backward curr next))
+      (backward-kill-symbolword-size next (incf num))
+    num)))
 
 (provide 'kill-symbolword)
