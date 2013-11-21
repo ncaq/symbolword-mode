5357f343e19b0de09c92ef850bc621bac716bdcd
ncaq <nyrigadake38@gmail.com>
2013-11-21 15:25:34 +0900

    upcaseの取り扱いがまずかったので,全面的に挙動を変えた. もっと高度な判定を行うことにした.

--- ward-symbolword.el
+++ ward-symbolword.el
@@ -1,17 +1,17 @@
 (require 'common-function)
 
-(defun forward-symbolword-sub (str)
-  (cond ((equal-syntax-and-cjk str (get-str-from-buffer)) (forward-char) (forward-symbolword-sub str))))
-
 (defun forward-symbolword ()
   (interactive)
-  (forward-symbolword-sub (get-str-from-buffer)))
-
-(defun backward-symbolword-sub (str)
-  (cond ((equal-syntax-and-cjk str (get-str-from-buffer-backward)) (backward-char) (backward-symbolword-sub str))))
+  (let ((curr (get-str-from-buffer)))
+    (forward-char)
+    (unless (div-symbolword-forward curr (get-str-from-buffer))
+      (forward-symbolword))))
 
 (defun backward-symbolword ()
   (interactive)
-  (backward-symbolword-sub (get-str-from-buffer-backward)))
+  (let ((curr (get-str-from-buffer-backward)))
+    (backward-char)
+    (unless (div-symbolword-backward curr (get-str-from-buffer-backward))
+      (backward-symbolword))))
 
 (provide 'ward-symbolword)
