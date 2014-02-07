773af5f5a35378310a00068d248fae6b4a6fd6bf
ncaq <ncaq@ncaq.net>
2014-02-08 03:01:23 +0900

    単語基準をEmacs標準に近いものにし,記号を単語とする以外には触らないようにした. ほぼ書き直しである.

--- ward-symbolword.el
+++ ward-symbolword.el
@@ -1,23 +1,40 @@
-(require 'common-function)
+(require 'symbolword-function)
+(require 'symbolword-rule)
 
 (defun forward-symbolword ()
   (interactive)
+  (forward-whitespace-and-newline)
   (forward-symbolword-sub (get-str-from-buffer)))
 
 (defun forward-symbolword-sub (curr)
   (let ((next (get-str-from-buffer)))
-    (unless (div-symbolword-forward curr next)
+    (if (not (div-symbolword-forward curr next))
+      (progn
+	(forward-char)
+	(forward-symbolword-sub next)))))
+
+(defun forward-whitespace-and-newline ()
+  (let ((currtype (unicode-block-type (get-str-from-buffer))))
+    (when (eq currtype 'space)
       (forward-char)
-      (forward-symbolword-sub next))))
+      (forward-whitespace-and-newline))))
 
 (defun backward-symbolword ()
   (interactive)
-  (backward-symbolword-sub (get-str-from-buffer-backward)))
+  (backward-whitespace-and-newline)
+  (backward-symbolword-sub (get-str-from-buffer-back)))
 
 (defun backward-symbolword-sub (curr)
-  (let ((back (get-str-from-buffer-backward)))
-    (unless (div-symbolword-backward curr back)
+  (let ((next (get-str-from-buffer-back)))
+    (if (not (div-symbolword-backward curr next))
+      (progn
+	(backward-char)
+	(backward-symbolword-sub next)))))
+
+(defun backward-whitespace-and-newline ()
+  (let ((currtype (unicode-block-type (get-str-from-buffer-back))))
+    (when (eq currtype 'space)
       (backward-char)
-      (backward-symbolword-sub back))))
+      (backward-whitespace-and-newline))))
 
 (provide 'ward-symbolword)
