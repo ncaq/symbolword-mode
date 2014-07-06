566fc19a0098c4a5e7803a8bfbef69fe02c90033
ncaq <ncaq@ncaq.net>
2014-07-07 01:50:46 +0900

    add support read-only property

--- symbolword-ward.el
+++ symbolword-ward.el
@@ -9,6 +9,10 @@
                    (symbolword/get-div-word-count-forward (1+ (point)))
                    (point)))))
 
+(defun symbolword/get-div-word-count-forward (n)
+  (if (symbolword/div-word-point? n)
+      n
+    (symbolword/get-div-word-count-forward (1+ n))))
 
 (defun backward-symbolword ()
   (interactive)
@@ -17,5 +21,10 @@
     (backward-char (-
                     (point)
                     (symbolword/get-div-word-count-backward (1- (point)))))))
+
+(defun symbolword/get-div-word-count-backward (n)
+  (if (symbolword/div-word-point? n)
+      n
+    (symbolword/get-div-word-count-backward (1- n))))
 
 (provide 'symbolword-ward)
