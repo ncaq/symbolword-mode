1e20c77c785bfefc81123954beaafa91d8f4215d
ncaq <ncaq@ncaq.net>
2014-07-07 01:50:46 +0900

    add support read-only property

--- symbolword-get.el
+++ symbolword-get.el
@@ -5,14 +5,4 @@
   (interactive)
   (symbolword/div-word? (char-before n) (char-after n)))
 
-(defun symbolword/get-div-word-count-forward (n)
-  (if (symbolword/div-word-point? n)
-      n
-    (symbolword/get-div-word-count-forward (1+ n))))
-
-(defun symbolword/get-div-word-count-backward (n)
-  (if (symbolword/div-word-point? n)
-      n
-    (symbolword/get-div-word-count-backward (1- n))))
-
 (provide 'symbolword-get)
--- symbolword-kill.el
+++ symbolword-kill.el
@@ -3,18 +3,28 @@
 
 (defun kill-symbolword ()
   (interactive)
-  (if (eobp)
+  (if (or (eobp) (get-text-property (1+ (point)) 'read-only))
       ()
     (kill-forward-chars (-
-                        (symbolword/get-div-word-count-forward (1+ (point)))
+                        (symbolword/get-div-word-count-forward-for-kill (1+ (point)))
                         (point)))))
+
+(defun symbolword/get-div-word-count-forward-for-kill (n)
+  (if (or (symbolword/div-word-point? n) (get-text-property n 'read-only))
+      n
+    (symbolword/get-div-word-count-forward (1+ n))))
 
 (defun backward-kill-symbolword ()
   (interactive)
-  (if (bobp)
+  (if (or (bobp) (get-text-property (1- (point)) 'read-only))
       ()
     (kill-backward-chars (-
                     (point)
                     (symbolword/get-div-word-count-backward (1- (point)))))))
+
+(defun symbolword/get-div-word-count-backward (n)
+  (if (or (symbolword/div-word-point? n) (get-text-property n 'read-only))
+      n
+    (symbolword/get-div-word-count-backward (1- n))))
 
 (provide 'symbolword-kill)
